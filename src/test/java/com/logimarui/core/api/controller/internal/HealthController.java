1a523d37c310aa6400724fe15357aa9622383e60
Luskahz <lucasalvesdemelolins@gmail.com>
2025-12-08 13:23:58 -0300

    [ Feat ] - Criando primeira rota básica, /health, verificação de estabilidade

---
+++ src/test/java/com/logimarui/core/api/controller/internal/HealthController.java
@@ -0,0 +1,12 @@
+package com.logimarui.core.api.controller.internal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public String health() {
+        return "ok";
+    }
+}
