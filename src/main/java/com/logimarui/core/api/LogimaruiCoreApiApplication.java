95c026c90029aa1bd17f678c61374e5804997acb
Luskahz <lucasalvesdemelolins@gmail.com>
2025-12-08 12:57:08 -0300

    [ Feat ] - Initializer, criação das pastas base do projeto e instalação das dependencias do maven

---
+++ src/main/java/com/logimarui/core/api/LogimaruiCoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.logimarui.core.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogimaruiCoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogimaruiCoreApiApplication.class, args);
+	}
+
+}
