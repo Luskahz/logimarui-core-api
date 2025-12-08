7bff36bfc7ae1d55eeac7608c1c4e047fed95104
Luskahz <lucasalvesdemelolins@gmail.com>
2025-12-08 16:58:04 -0300

    [ Feat ] - Configurando logica para 2 bancos de dados read e write

--- src/main/java/com/logimarui/core/api/LogimaruiCoreApiApplication.java
+++ src/main/java/com/logimarui/core/api/LogimaruiCoreApiApplication.java
@@ -1,13 +1,14 @@
 package com.logimarui.core.api;
 
+import com.logimarui.core.api.config.db.ReadDbProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(ReadDbProperties.class)
 public class LogimaruiCoreApiApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(LogimaruiCoreApiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(LogimaruiCoreApiApplication.class, args);
+    }
 }
---
+++ src/main/java/com/logimarui/core/api/config/db/WriteDbConfig.java
@@ -0,0 +1,4 @@
+package com.logimarui.core.api.config.db;
+
+public class WriteDbConfig {
+}
